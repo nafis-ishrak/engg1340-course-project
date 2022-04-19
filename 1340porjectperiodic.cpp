9d6906ad8afcd5a9955911eaa9db1d96c57f310e
Syed Nafis Ishrak <syednafisishrak@Syeds-MacBook-Air.local>
2022-04-19 15:57:02 +0800

    periodic table initial

---
+++ 1340porjectperiodic.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cctype>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+// For chemistry
+using namespace std;
+
+struct element
+{
+  string symbol;
+  int atomicWeight;
+  int group;
+  int period;
+};
+
+void elements(element arr[])
+{
+  element arr[50] = {{"H ",1, 1, 1} , {"He" ,2, 18, 1}, {"Li", 3, 1, 2}, Be 4 2 2 B 5 13 2 "C ",6 14 2 N 7 15 2 O 8 16 2 F 9 17 2 Ne 10 18 2 Na 11 1 3 Mg 12 2 3 Al 13 13 3 Si 14 14 3 P 15 15 3 S 16 16 3 Cl 17 17 3 Ar 18 18 3 K 19 1 4 Ca 20 2 4  Ga 31 13 4 Ge 32 14 4 As 33 15 4 Se 34 16 4 Br 35 17 4 Kr 36 18 4 Rb 37 1 5 Sr 38 2 5  In 49 13 5 Sn 50 14 5 Sb 51 15 5 Te 52 16 5 I 53 17 5 Xe 54 18 5 Cs 55 1 6 Ba 56 2 6 Tl 81 13 6 Pb 82 14 6 Bi 83 15 6 Po 84 16 6 At 85 17 6 Rn 86 18 6 Fr 87 1 7 Ra 88 2 7  Nh 113 13 7 Fl 114 14 7 Mc 115 15 7 Lv 116 16 7 Ts 117 17 7 Og 118 18 7  }};
+
+int main()
+{
+
+  return 0;
+}
